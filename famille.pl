9fcd76675b5060e8df2a9cb98024fbf4fc0abd22
Rémi Martin <mrmi11@yahoo.fr>
2014-10-03 09:26:37 +0200

    TP - First part

---
+++ famille.pl
@@ -0,0 +1,33 @@
+woman(alicia).
+woman(justine).
+woman(cecilia).
+woman(camille).
+
+man(remi).
+man(felipe).
+man(titouan).
+man(alexandre).
+
+% parent(parent,child).
+parent(alexandre, alicia).
+parent(camille, alicia).
+parent(felipe,titouan).
+parent(cecilia,titouan).
+parent(remi,alexandre).
+parent(remi,camille).
+parent(remi,felipe).
+parent(felipe,justine).
+parent(cecilia,justine).
+
+% ancester(ancester,child).
+ancester(X,Y) :- parent(X,Y).
+ancester(X,Y) :- parent(X,Z), ancester(Z,Y), X \= Y.
+
+% fraternity(one,two).
+fraternity(X,Y) :- parent(Z,X), parent(Z,Y), X \= Y.
+
+% child(child,parent).
+child(X,Y) :- parent(Y,X).
+
+% uncle(uncle,nephew).
+uncle(X,Y) :- parent(Z,Y), fraternity(Z,X), X \= Y.
