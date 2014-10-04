e3901222379b70917a760c440c467ed6876dbda2
RemiMartin <mrmi11@yahoo.fr>
2014-10-04 17:48:42 +0200

    debugged member\3

--- felipe-remi/lists.pl
+++ felipe-remi/lists.pl
@@ -1,7 +1,3 @@
-member(X,[X|_]).
-member(X,[_|L]) :- member(X,L).
-
-member(X,[],[]).
-member(X,[X],[]).
-member(X,[X|Q],Q) :- member(X,Q,Q).
+member(X,[X],[]):-!.
+member(X,[X|Q],Q).
 member(X,[T|L],[T|R]) :- member(X,L,R).
