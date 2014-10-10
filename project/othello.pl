315884baf892f47c6fcabb7deb1ce118fdf31490
Alexandre Sarazin <alexandre.sarazin1@gmail.com>
2014-10-10 09:32:55 +0200

    New board + structure of game

--- project/othello.pl
+++ project/othello.pl
@@ -1,1 +1,5 @@
 :- include('init.pl').
+
+play(Player) :- win(???).
+play(Player) :- board(X), nextMove(X,Player,Move), update(X,Player,Move,NewBoard), retract(board(X)), assert(board(NewBoard)), nextPlayer(Player,NewPlayer), play(NewPlayer).
+
