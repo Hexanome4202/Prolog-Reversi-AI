3c655415d5b6ca16448d506b39a006e8469f1af8
Rémi Martin <mrmi11@yahoo.fr>
2014-10-10 09:49:48 +0200

    Updates nextMove and ia predicates

--- project/nextMove.pl
+++ project/nextMove.pl
@@ -1,3 +1,4 @@
 % nextMove(X,Player,Move).
-nextMove(X,1,Move) :- ia1(X,Move).
-nextMove(X,2,Move) :- ia2(X,Move).
+nextMove(X,Player,Move) :- nextMove(X,Player,Moves,Move).
+nextMove(X,1,Moves,Move) :- ia1(X,Moves,Move).
+nextMove(X,2,Moves,Move) :- ia2(X,Moves,Move).
