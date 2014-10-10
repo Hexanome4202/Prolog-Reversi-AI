b5a478ddc585dabf984bd27f32dfd6eed9a3d679
Alexandre Sarazin <alexandre.sarazin1@gmail.com>
2014-10-10 09:46:14 +0200

    Added predicate nextPlayer

--- project/othello.pl
+++ project/othello.pl
@@ -3,3 +3,4 @@
 play(Player) :- win(???).
 play(Player) :- board(X), nextMove(X,Player,Move), update(X,Player,Move,NewBoard), retract(board(X)), assert(board(NewBoard)), nextPlayer(Player,NewPlayer), play(NewPlayer).
 
+nextPlayer(Player,NewPlayer) :- P1 is Player+1, pow(-1,P1,X), NewPlayer is Player+X.
