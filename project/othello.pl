4ad5da27e3e709cd58918ba08e240af361ab3a98
Alexandre Sarazin <alexandre.sarazin1@gmail.com>
2014-10-10 11:13:26 +0200

    Added win predicate + possibleMoves global variables

--- project/othello.pl
+++ project/othello.pl
@@ -1,7 +1,8 @@
 :- include('init.pl').
+:- include('nextMove.pl').
 
-play(Player) :- win(???).
-play(Player) :- board(X), nextMove(X,Player,Move), update(X,Player,Move,NewBoard), retract(board(X)), assert(board(NewBoard)), nextPlayer(Player,NewPlayer), play(NewPlayer).
+play(Player,Winner) :-  win(Winner),!.
+play(Player,Winner) :- board(X), nextMove(X,Player,Move), update(X,Player,Move,NewBoard), retract(board(X)), assert(board(NewBoard)), nextPlayer(Player,NewPlayer), play(NewPlayer).
 
 % nextPlayer(Player, NewPlayer) -> 1 gives 2, 2 gives 1.
 nextPlayer(Player,NewPlayer) :- P1 is Player+1, pow(-1,P1,X), NewPlayer is Player+X.
@@ -16,3 +17,11 @@ sizeOfList([_|T],N) :- sizeOfList(T,N1), N is N1+1.
 
 % randomIA(PossibleMoves, Move)
 randomIA(PossibleMoves, Move) :- sizeOfList(PossibleMoves, Size), Position is random(Size)+1, elementAt(MoveList,PossibleMoves,Position), elementAt(Move,MoveList,1).
+
+% win -> conditions to win
+win(Winner) :- possibleMoves1(X), X = [], possibleMoves2(Y), Y = [], board(Board), count(1,Board, C1), count(2,Board,C2), (C1 > C2 -> Winner is 1; (C2 > C1 -> Winner is 2 ; Winner is 0)).
+
+% count(Element, List, Counter) 
+count(Element, List, Counter) :- count(Element, List, Counter, 0).
+count(Element, [], Counter, Counter).
+count(Element, [Head|Tail], Counter, ACounter) :- (Element = Head -> C1 is ACounter+1 ; C1 is ACounter), count(Element,Tail,Counter,C1).
