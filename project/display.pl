6fb327fddba03d78f5281ae7df0104666390b641
RemiMartin <mrmi11@yahoo.fr>
2014-10-17 08:16:33 +0200

    Changed display

--- project/display.pl
+++ project/display.pl
@@ -1,4 +1,4 @@
 % display(Board);
 display(Board) :- display(Board,1).
-display([],Pos).
-display([X|Board],Pos) :- write(X), Mod is Pos mod 8, (Mod == 0 -> nl ; write(' ')), Pos1 is Pos + 1, display(Board,Pos1).
+display([],_).
+display([X|Board],Pos) :- (X == 1 -> write('X'); (X == 2 -> write('O'); write(' '))), Mod is Pos mod 8, (Mod == 0 -> nl ; write('|')), Pos1 is Pos + 1, display(Board,Pos1).
