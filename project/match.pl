119317aee8307fb9f261b33141f61d737b0c908f
Cécilia Van Bever <ceciliavanbever@gmail.com>
2014-10-17 08:58:35 +0200

    Fixed match predicate. Fixed Update and Match Warnings.

--- project/match.pl
+++ project/match.pl
@@ -1,6 +1,8 @@
-match(List, Player, Points) :- match2(List, Player, 0, Points).
+%Gets how many points are got with the given sublist
+match(List, Player, Points) :- match(List, Player, 0, Points).
 
-match2([],  Player, TempPoints, Points) :- Points is TempPoints, TempPoints > 0.
-match2([X|Y], Player, TempPoints, Points) :- X==0, Points is 0. 
-match2([X|Y], Player, TempPoints, Points) :- X==Player, Points is TempPoints, TempPoints > 0.
-match2([X|Y], Player, TempPoints, Points) :- X>0, X =\= Player, match2(Y, Player, TempPoints+1, Points).
+match([],  _, TempPoints, Points) :- Points is TempPoints ,!.
+match([X], Player, _, Points) :- X =\= Player, Points is 0.
+match([X|_], _, _, Points) :- X==0, Points is 0, !. 
+match([X|_], X, TempPoints, Points) :- Points is TempPoints, !.
+match([X|Y], Player, TempPoints, Points) :- X>0, X =\= Player, match(Y, Player, TempPoints+1, Points).
--- project/update.pl
+++ project/update.pl
@@ -1,5 +1,7 @@
 append([ ], L1, L1).
 append([A|L1],L2,[A|L3]) :- append(L1,L2,L3).
 
-update([X|Y],Player,1, [Player|Y] ).
+%Replaces the piece at Position by a Player's piece
+update([_|Y],Player,1, [Player|Y] ).
 update([X|Y], Player, Move, NewBoard) :- NewMove is Move-1, update(Y, Player, NewMove, Test), append([X], Test, NewBoard).
+
