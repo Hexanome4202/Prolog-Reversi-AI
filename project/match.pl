39876c65b34229dafcd7f6bf61f3a0ee7e8b2d6d
Alexandre Sarazin <alexandre.sarazin1@gmail.com>
2014-10-17 08:31:27 +0200

    Removed other singleton variables

--- project/match.pl
+++ project/match.pl
@@ -1,6 +1,6 @@
 match(List, Player, Points) :- match2(List, Player, 0, Points).
 
-match2([],  Player, TempPoints, Points) :- Points is TempPoints, TempPoints > 0.
-match2([X|Y], Player, TempPoints, Points) :- X==0, Points is 0. 
-match2([X|Y], Player, TempPoints, Points) :- X==Player, Points is TempPoints, TempPoints > 0.
+match2([],_, TempPoints, Points) :- Points is TempPoints, TempPoints > 0.
+match2([X|_],_, _, Points) :- X==0, Points is 0. 
+match2([X|_], Player, TempPoints, Points) :- X==Player, Points is TempPoints, TempPoints > 0.
 match2([X|Y], Player, TempPoints, Points) :- X>0, X =\= Player, match2(Y, Player, TempPoints+1, Points).
