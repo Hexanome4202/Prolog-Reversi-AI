86b40701380c8865da9a43b1f5d04004174e2922
Titouan Thibaud <titouan.thibaud@gmail.com>
2014-10-23 22:48:38 +0200

    Added stats predicates

--- project/stats.pl
+++ project/stats.pl
@@ -1,6 +1,11 @@
 % add the result of the game to the result file
-addResult(Result) :- open('stats.txt', append, Handle), write(Handle, Result), write(Handle, ';'), close(Handle).
+addResult(Result) :- open('stats.txt', append, Handle), write(Handle, Result), write(Handle, '\n'), close(Handle).
 
 %stats(NbIterations)
-stats(0) :- !.
-stats(Restant) :- play(Winner), addResult(Winner), I is Restant-1, stats(I).
+stats(_, _, 0) :- !.
+stats(Ia1, Ia2, Restant) :- play(Winner, Ia1, Ia2), addResult(Winner), I is Restant-1, stats(Ia1, Ia2, I).
+
+listeStats(0, _, _) :- !.
+listeStats(Ia1, Ia1, Nb) :- NewIa2 is Ia2 - 1, listeStats(Ia1, NewIa2).
+listeStats(Ia1, 0, Nb) :- NewIa1 is Ia1 -1, listeStats(NewIa1, 4, Nb).
+listeStats(Ia1, Ia2, Nb) :- stats(Ia1, Ia2, Nb), NewIa2 is Ia2 - 1, listeStats(Ia1, NewIa2).
